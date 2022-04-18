78960fa4d20682165f149c8c3e54bc8cc488ebb2
Microsoft Windows 11 Insider Preview Development Korea <60504748+korea414@users.noreply.github.com>
2022-04-18 10:47:43 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
-include <stdio.h>
-void main(){
-	printf("hello world!" \d);
-	};
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+void main()
+{
+    srand(10);
+    printf("난수 : %d\n", rand() % 10);
+    printf("난수 : %d\n", rand() % 10);
+    printf("난수 : %d\n", rand() % 10);
+    printf("난수 : %d\n", rand() % 10);
+}
